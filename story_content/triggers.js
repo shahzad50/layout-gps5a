d7c21903ad1f626611b591de7c0ee4f7cb3efc49
shahzad50 <95250613+shahzad50@users.noreply.github.com>
2024-11-30 07:30:09 +0500

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,37 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6iKbVWcN28c":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  const target = object('5eOML9NGrQG');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6LJLJMmQeQ3';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
